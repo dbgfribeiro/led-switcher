f5a905e9e0c3bb0482ceacd973f8aad50f462e41
Diogo Ribeiro <diogoribeiro@redlightsoft.com>
2022-06-03 18:57:20 +0100

    final standalone test

--- js/stay_standalone.js
+++ js/stay_standalone.js
@@ -1,16 +1,14 @@
-// if (('standalone' in navigator) && navigator.standalone) {
-//   document.addEventListener('click', function(e) {
-//     var curnode = e.target
-//     while (!(/^(a|html)$/i).test(curnode.nodeName)) {
-//       curnode = curnode.parentNode
-//     }
-//     if ('href' in curnode
-//       && (chref = curnode.href).replace(document.location.href, '').indexOf('#')
-//       && (!(/^[a-z\+\.\-]+:/i).test(chref)
-//       || chref.indexOf(document.location.protocol + '//' + document.location.host) === 0)
-//     ) {
-//       e.preventDefault()
-//       document.location.href = curnode.href
-//     }
-//   }, false)
-// }
+function escapeRegExp(string){
+  return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+}
+
+if (window.navigator.standalone) {
+  var local = document.domain;
+  $(document).on('click', 'a', function(event) {
+      var a = $(this).attr('href');
+      if ( a.match('https?://(www\\.)?' + escapeRegExp(local))){
+          event.preventDefault();
+          document.location.href = a;
+      }
+  });
+}
